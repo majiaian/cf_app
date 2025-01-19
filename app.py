2b6e9784a663d1d6044eb8e02a54870f790d3ad7
majiaian <55089567+majiaian@users.noreply.github.com>
2025-01-19 23:21:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ page1 = st.Page("pages/1_Antibio.py", title="抗菌药物")
 page2 = st.Page("pages/2_PPI.py", title="PPI")
 page3 = st.Page("pages/3_Albumin.py", title="人血白蛋白")
 pages = {
-    "辅助决策": [page1, page2，page3],
+    "辅助决策": [page1, page2,page3],
     "公式工具": [],
 }
 
