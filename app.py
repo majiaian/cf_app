24f0ebb0e22b8882c57a9cd9c5d609d4a47d5550
majiaian <55089567+majiaian@users.noreply.github.com>
2025-01-23 10:08:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="药物辅助决策工具",initial_sidebar_state="
 page1 = st.Page("pages/1_Antibio.py", title="抗菌药物")
 page2 = st.Page("pages/2_PPI.py", title="PPI")
 page3 = st.Page("pages/3_Albumin.py", title="人血白蛋白")
-page4 = st.Page("pages/5_QTc.py", title="QTc计算")
+page4 = st.Page("pages/5_QTc.py")
 page5 = st.Page("pages/6_CURB65.py", title="CURB65肺炎评分")
 page6 = st.Page("pages/4_OPTCAD.py", title="OPTCAD心肌缺血评分")
 pages = {
